430e7e750af922739aff82f5b96fe0a601c259da
Michael Larsen <michael@larsen.be>
2026-01-15 12:39:03 +0100

    Update create_obs_DB.py

--- backend/create_obs_DB.py
+++ backend/create_obs_DB.py
@@ -1,4 +1,4 @@
 import sqlite3
 con = sqlite3.connect("stock.db")
 cur = con.cursor()
-cur.execute("CREATE TABLE ObsStocks(Name,Ticker,BuyTarget,CurrencyId,Lables,Comment)")
+cur.execute("CREATE TABLE ObsStocks(Name,Ticker,BuyTarget,CurrencyId,Lables,Comment,Evaluated)")
