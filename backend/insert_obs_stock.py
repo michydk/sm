858a46d52705dd81412d039b81a6fd6b5e149df6
Michael Larsen <michael@larsen.be>
2026-01-15 10:01:22 +0100

    Create insert_obs_stock.py

---
+++ backend/insert_obs_stock.py
@@ -0,0 +1,20 @@
+import sqlite3
+import sys
+
+if len(sys.argv) != 7:
+  count = str(len(sys.argv))
+  sys.exit("Not 6 args (foundt " + count + "), syntax is: 'Name' 'Ticker' 'BuyTarget' 'CurrencyId' 'Lables' 'Comment'")
+ca_name = str(sys.argv[1])
+ca_ticker = str(sys.argv[2])
+ca_buytaget = str(sys.argv[3])
+ca_currencyid = str(sys.argv[4])
+ca_lables = str(sys.argv[5])
+ca_comment = str(sys.argv[6])
+
+insert = """INSERT INTO ObsStocks (Name,Ticker,BuyTarget,CurrencyId,Lables,Comment) VALUES (?, ?, ?, ?, ?, ?); """
+insert_values = (ca_name, ca_ticker, ca_buytaget, ca_currencyid, ca_lables, ca_comment)
+
+con = sqlite3.connect("stock.db")
+cur = con.cursor()
+cur.execute(insert, insert_values)
+con.commit()
