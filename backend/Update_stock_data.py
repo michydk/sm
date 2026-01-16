6419c4cb7e96a52cf5b2152cc9d93a944e2f9b72
Michael Larsen <michael@larsen.be>
2026-01-16 16:26:49 +0100

    Update Update_stock_data.py

--- backend/Update_stock_data.py
+++ backend/Update_stock_data.py
@@ -1,6 +1,32 @@
 import sqlite3
 import datetime
 import yfinance as yf
+from scw_serverless import Serverless
+import boto3
+
+#https://stocks.s3.nl-ams.scw.cloud/stock.db
+
+REGION = "fr-par"
+S3_URL = "https://s3.nl-ams.scw.cloud"
+
+def handle(event, context):
+
+s3 = boto3.client(
+        "s3",
+        region_name=REGION,
+        use_ssl=True,
+        endpoint_url=S3_URL,
+        aws_access_key_id=SCW_ACCESS_KEY,
+        aws_secret_access_key=SCW_SECRET_KEY,
+    )
+
+logging.info(f"Uploading to {BUCKET_NAME}/{name}")
+    s3.put_object(Bucket=BUCKET_NAME, Key=name, Body=target.value)
+
+    return {"statusCode": 200, "body": f"Successfully uploaded {name} to bucket!"}
+
+
+
 
 tickercon = sqlite3.connect("stock.db")
 tickercur = tickercon.cursor()
